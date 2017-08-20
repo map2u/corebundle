2572183fa27c0d0f4ec4627ad70f5f13d8858982
Joseph Zhao <zhaoshuilin2004@yahoo.ca>
2017-08-19 23:07:08 -0400

    initial

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Map2u\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('Map2uCoreBundle:Default:index.html.twig');
+    }
+}
